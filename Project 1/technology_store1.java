42feff3db782d2e36ce648f0522bea55e96b29ab
Berkant-Simsek <126967154+Berkant-Simsek@users.noreply.github.com>
2023-03-11 19:02:29 +0300

    Create technology_store1.java

---
+++ Project 1/technology_store1.java
@@ -0,0 +1,11 @@
+package technology_store;
+
+public abstract class technology_store1 implements technology_store {
+	public String name_or_brand_name;
+	
+	@Override
+	public void enter() {
+		System.out.println("entered.");
+	}
+	
+}
