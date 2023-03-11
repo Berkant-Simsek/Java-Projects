ffa73831d2fa665eccbbfbd3ec111fe8f9a06a65
Berkant-Simsek <126967154+Berkant-Simsek@users.noreply.github.com>
2023-03-11 19:07:15 +0300

    Create humans.java

---
+++ Project 1/humans.java
@@ -0,0 +1,12 @@
+package technology_store;
+
+public abstract class humans extends technology_store1 {
+	public String name_or_brand_name;
+	protected String superscription;
+	
+	@Override
+	public void enter() {
+		System.out.println("entered.");
+	}
+	
+}
